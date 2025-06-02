c24c253faf536639c0701f64a947302877328540
LucasLopez1 <lucaslopez@udesa.edu.ar>
2025-06-02 13:27:54 -0300

    Termine index.js

--- js/index.js
+++ js/index.js
@@ -35,4 +35,62 @@ window.addEventListener("load", function () {
       listaMejorCalificadas.innerHTML = contenido;
     });
 
+// Series populares
+  fetch(urlSeries)
+    .then(function (respuesta) {
+      return respuesta.json();
+    })
+    .then(function (datos) {
+      var contenido = "";
+      for (var i = 0; i < 5; i++) {
+        var serie = datos.results[i];
+        var titulo = serie.name;
+        var fecha = serie.first_air_date;
+        var imagen = "https://image.tmdb.org/t/p/w500" + serie.poster_path;
+        var id = serie.id;
+
+
+        contenido += '<li>' +
+          '<nav>' +
+          '<h3>' + titulo + '</h3>' +
+          '<a href="detail-series.html?id=' + id + '">' +
+          '<img src="' + imagen + '" alt="' + titulo + '">' +
+          '</a>' +
+          '<h3>' + fecha + '</h3>' +
+          '</nav>' +
+          '</li>';
+      }
+      listaSeriesPopulares.innerHTML = contenido;
+    });
+
+
+  // Películas más vistas
+  fetch(urlPeliculas)
+    .then(function (respuesta) {
+      return respuesta.json();
+    })
+    .then(function (datos) {
+      var contenido = "";
+      for (var i = 0; i < 5; i++) {
+        var pelicula = datos.results[i];
+        var titulo = pelicula.title;
+        var fecha = pelicula.release_date;
+        var imagen = "https://image.tmdb.org/t/p/w500" + pelicula.poster_path;
+        var id = pelicula.id;
+
+
+        contenido += '<li>' +
+          '<nav>' +
+          '<h3>' + titulo + '</h3>' +
+          '<a href="detail-movies.html?id=' + id + '">' +
+          '<img src="' + imagen + '" alt="' + titulo + '">' +
+          '</a>' +
+          '<h3>' + fecha + '</h3>' +
+          '</nav>' +
+          '</li>';
+      }
+      listaPeliculasPopulares.innerHTML = contenido;
+    });
+
+
 });
