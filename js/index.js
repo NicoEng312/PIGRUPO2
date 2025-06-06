0ffc607d98473625e88f2f754464971ec645ecf7
LucasLopez1 <lucaslopez@udesa.edu.ar>
2025-06-06 15:09:31 -0300

    Index.js corregido y terminado

--- js/index.js
+++ js/index.js
@@ -11,57 +11,67 @@ window.addEventListener("load", function () {
   // Películas mejor calificadas
   fetch(urlMejorCalificadas)
     .then(function (respuesta) {
-      return respuesta.json();
+        return respuesta.json();
     })
     .then(function (datos) {
-      let contenido = "";
-      for (let i = 0; i < 5; i++) {
-        let pelicula = datos.results[i];
-        let titulo = pelicula.title;
-        let fecha = pelicula.release_date;
-        let imagen = "https://image.tmdb.org/t/p/w500" + pelicula.poster_path;
-        let id = pelicula.id;
+        let contenido = "";
+        for (let i = 0; i < datos.results.length; i = i + 1) {
+          if (i >= 10) {
+            break;
+          }
+          const pelicula = datos.results[i];
+          let titulo = pelicula.title;
+          let fecha = pelicula.release_date;
+          let imagen = `https://image.tmdb.org/t/p/w500${pelicula.poster_path}`;
+          let id = pelicula.id;
 
-        contenido += '<li>' +
-          '<nav>' +
-          '<h3>' + titulo + '</h3>' +
-          '<a href="detail-movies.html?id=' + id + '">' +
-          '<img src="' + imagen + '" alt="' + titulo + '">' +
-          '</a>' +
-          '<h3>' + fecha + '</h3>' +
-          '</nav>' +
-          '</li>';
-      }
-      listaMejorCalificadas.innerHTML = contenido;
-    });
+          contenido += `
+            <li>
+              <nav>
+                <h3>${titulo}</h3>
+                <a href="detail-movies.html?id=${id}">
+                  <img src="${imagen}" alt="${titulo}">
+                </a>
+                <h3>${fecha}</h3>
+              </nav>
+            </li>
+          `;
+        }
+        listaMejorCalificadas.innerHTML = contenido;
+      });
 
-  // Series populares
-  fetch(urlSeries)
+ // Series populares
+ fetch(urlSeries)
     .then(function (respuesta) {
-      return respuesta.json();
+        return respuesta.json();
     })
     .then(function (datos) {
       let contenido = "";
-      for (let i = 0; i < 5; i++) {
-        let serie = datos.results[i];
+      for (let i = 0; i < datos.results.length; i = i + 1) {
+        if (i >= 10) {
+          break;
+        }
+        const serie = datos.results[i];
         let titulo = serie.name;
         let fecha = serie.first_air_date;
-        let imagen = "https://image.tmdb.org/t/p/w500" + serie.poster_path;
+        let imagen = `https://image.tmdb.org/t/p/w500${serie.poster_path}`;
         let id = serie.id;
 
-        contenido += '<li>' +
-          '<nav>' +
-          '<h3>' + titulo + '</h3>' +
-          '<a href="detail-series.html?id=' + id + '">' +
-          '<img src="' + imagen + '" alt="' + titulo + '">' +
-          '</a>' +
-          '<h3>' + fecha + '</h3>' +
-          '</nav>' +
-          '</li>';
+        contenido += `
+          <li>
+            <nav>
+              <h3>${titulo}</h3>
+              <a href="detail-series.html?id=${id}">
+                <img src="${imagen}" alt="${titulo}">
+              </a>
+              <h3>${fecha}</h3>
+            </nav>
+          </li>
+        `;
       }
       listaSeriesPopulares.innerHTML = contenido;
     });
-
+   
   // Películas más vistas
   fetch(urlPeliculas)
     .then(function (respuesta) {
@@ -69,22 +79,27 @@ window.addEventListener("load", function () {
     })
     .then(function (datos) {
       let contenido = "";
-      for (let i = 0; i < 5; i++) {
-        let pelicula = datos.results[i];
+      for (let i = 0; i < datos.results.length; i = i + 1) {
+        if (i >= 10) {
+          break;
+        }
+        const pelicula = datos.results[i];
         let titulo = pelicula.title;
         let fecha = pelicula.release_date;
-        let imagen = "https://image.tmdb.org/t/p/w500" + pelicula.poster_path;
+        let imagen = `https://image.tmdb.org/t/p/w500${pelicula.poster_path}`;
         let id = pelicula.id;
 
-        contenido += '<li>' +
-          '<nav>' +
-          '<h3>' + titulo + '</h3>' +
-          '<a href="detail-movies.html?id=' + id + '">' +
-          '<img src="' + imagen + '" alt="' + titulo + '">' +
-          '</a>' +
-          '<h3>' + fecha + '</h3>' +
-          '</nav>' +
-          '</li>';
+        contenido += `
+          <li>
+            <nav>
+              <h3>${titulo}</h3>
+              <a href="detail-movies.html?id=${id}">
+                <img src="${imagen}" alt="${titulo}">
+              </a>
+              <h3>${fecha}</h3>
+            </nav>
+          </li>
+        `;
       }
       listaPeliculasPopulares.innerHTML = contenido;
     });
