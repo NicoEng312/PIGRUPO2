b07aa4c74949ba3910eed8723dd36762e191912d
NicoEng312 <nengelhard@udesa.edu.ar>
2025-06-06 18:05:14 -0300

    corregi el movie y series genres

--- js/movie-genres.js
+++ js/movie-genres.js
@@ -1,30 +1,45 @@
 window.addEventListener("load", function () {
   const apiKey = '758f9c0fe9cf446d2c9eb164921c167f';
-  let listaGeneros = document.querySelector(".listaz");
-  let urlGeneros = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=es-ES`;
-  let allowedGenres = ['Acción', 'Terror', 'Ciencia ficción', 'Comedia', 'Suspense'];
+  const genreList = document.querySelector(".listaz");
 
-  fetch(urlGeneros)
+
+  let genresUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=es-ES`;
+
+
+  fetch(genresUrl)
     .then(function (respuesta) {
       return respuesta.json();
     })
     .then(function (datos) {
       let contenido = "";
-      let addedGenres = new Set();
-      for (let i = 0; i < datos.genres.length; i++) {
-        let genero = datos.genres[i];
-        if (allowedGenres.includes(genero.name) && !addedGenres.has(genero.name)) {
-          contenido += '<li class="zzzj">' +
-            '<nav>' +
-              '<a href="detail-genres.html?id=' + genero.id + '&name=' + encodeURIComponent(genero.name) + '">' +
-                '<h3>' + genero.name + '</h3>' +
-              '</a>' +
-            '</nav>' +
-          '</li>';
-          addedGenres.add(genero.name);
+      let allowedGenres = ['Acción', 'Terror', 'Ciencia ficción', 'Comedia', 'Suspense'];
+
+
+      for (let i = 0; i < datos.genres.length; i = i + 1) {
+        let genre = datos.genres[i];
+        let permitido = false;
+        for (let j = 0; j < allowedGenres.length; j++) {
+          if (genre.name === allowedGenres[j]) {
+            permitido = true;
+          }
+        }
+        let yaAgregado = false;
+        for (let k = 0; k < i; k++) {
+          if (datos.genres[k].name === genre.name) {
+            yaAgregado = true;
+          }
+        }
+        if (permitido && !yaAgregado) {
+          contenido += `<li class="zzzj">
+            <nav>
+              <a href="detail-genres.html?id=${genre.id}&name=${encodeURIComponent(genre.name)}">
+                <h3>${genre.name}</h3>
+              </a>
+            </nav>
+          </li>`;
         }
       }
-      listaGeneros.innerHTML = contenido;
+      genreList.innerHTML = contenido;
     })
     .catch(function (error) {
       console.error("Error al obtener los géneros:", error);
